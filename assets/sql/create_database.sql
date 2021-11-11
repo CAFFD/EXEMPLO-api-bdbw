eae6f9cdb5f47bc6828a7a4ba2d6b5bb57c1ba67
Gabriel Guarsoni Dadário <gabrielguarsoni.4664@gmail.com>
2021-11-11 20:09:48 +0000

    11-11-21

--- assets/sql/create_database.sql
+++ assets/sql/create_database.sql
@@ -1,2 +1,10 @@
 -- Código SQL para a criação da estrutura do banco de dados
 -- Lembre-se de alterar a configuração 'dbname' ('config/database.php') para refletir o nome do banco criado
+
+create database exemplo;
+use exemplo;
+
+create table filmes(
+    id int auto_increment primary key,
+    titulo varchar(255)
+);
--- assets/sql/init_data.sql
+++ assets/sql/init_data.sql
@@ -1,1 +1,6 @@
 -- Código SQL com a inclusão dos registros iniciais do banco de dados
+
+insert into filmes (titulo) values
+    ('Entre Facas e Segredos'),
+    ('A Sociedade do Anel'),
+    ('O Hobbit');
